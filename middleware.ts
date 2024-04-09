d959bb128bce8e6ba5d1916778e7f6aaff724f0b
kaarthi290598 <kaarthi.work@gmail.com>
2024-04-09 21:04:11 +0530

    clerk changes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { Webhook } from "lucide-react";
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/api/webhook/clerk"],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
