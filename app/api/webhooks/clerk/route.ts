98273ba2b40e9268089b8669e227358e3846dd96
kaarthi290598 <kaarthi.work@gmail.com>
2024-04-09 23:04:42 +0530

    imagnify 2

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -61,28 +61,28 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
-    console.log("Test");
-    // const user = {
-    //   clerkId: id,
-    //   email: email_addresses[0].email_address,
-    //   username: username!,
-    //   firstName: first_name,
-    //   lastName: last_name,
-    //   photo: image_url,
-    // };
 
-    // const newUser = await createUser(user);
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    };
 
-    // // Set public metadata
-    // if (newUser) {
-    //   await clerkClient.users.updateUserMetadata(id, {
-    //     publicMetadata: {
-    //       userId: newUser._id,
-    //     },
-    //   });
-    // }
+    const newUser = await createUser(user);
 
-    // return NextResponse.json({ message: "OK", user: newUser });
+    // Set public metadata
+    if (newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id,
+        },
+      });
+    }
+
+    return NextResponse.json({ message: "OK", user: newUser });
   }
 
   // UPDATE
